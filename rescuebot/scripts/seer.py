90e9317c840190afeed18cc418bd9a88a89bc5d2
vpreston <victorialynnpreston@gmail.com>
2014-11-29 19:34:29 -0500

    Can see and identify all colored balls

--- rescuebot/scripts/seer.py
+++ rescuebot/scripts/seer.py
@@ -11,36 +11,88 @@ def find_circles(img_src,img_out):
     circles = cv2.HoughCircles(img_src, cv2.cv.CV_HOUGH_GRADIENT, 1, img_src.shape[0]/8, param1=10, param2=30, minRadius=50, maxRadius=75)
     hsv_img = cv2.cvtColor(img_out, cv2.COLOR_BGR2HSV)
 
-    lower_red = np.array([0, 55, 55])
-    upper_red = np.array([0, 255, 255])
-    mask = cv2.inRange(hsv_img, lower_red, upper_red)
+    lower_red = np.array([150, 55, 55])
+    upper_red = np.array([200, 255, 255])
+    mask_red = cv2.inRange(hsv_img, lower_red, upper_red)
+
+    lower_blue = np.array([100, 55, 55])
+    upper_blue = np.array([150, 255, 255])
+    mask_blue = cv2.inRange(hsv_img, lower_blue, upper_blue)
+
+    lower_yellow = np.array([20, 55,55])
+    upper_yellow = np.array([40, 255, 255])
+    mask_yellow = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
+
+    lower_green = np.array([60, 55, 55])
+    upper_green = np.array([100, 255, 255])
+    mask_green = cv2.inRange(hsv_img, lower_green, upper_green)
 
     location = None
+    global ball_location
     if circles is not None:
     	for c in circles[0,:]:
-	    	ROI = mask[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
-	      	mean = cv2.mean(ROI)
-	      	# print mean
-	      	cv2.imshow('mask', mask)
-	      	# cv2.imshow('canny', img_src)
-	      # 	if cv2.waitKey(10) & 0xFF == ord('q'):
-		    	# cap.release()
-		    	# cv2.destroyAllWindows()   
-	      	cv2.circle(img_out,(c[0],c[1]),c[2],(0,255,0),1)
-	      	# draw the center of the circle
-	      	cv2.circle(img_out,(c[0],c[1]),2,(0,0,255),3)
-	      	if mean[0] > 10:
-	      		print mean
-	      	#if mask[c[1], c[0]]  > 100:
-	      		cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (255,0,0), 2)
-		    	# draw the outer circle
-		    	cv2.circle(img_out,(c[0],c[1]),c[2],(255,0,0),5)
-		        # draw the center of the circle
-		    	cv2.circle(img_out,(c[0],c[1]),2,(0,0,255),3)
-		    	location = Vector3(c[0], c[1],c[2])
-		        #print (c[0],c[1],c[2])
-    global ball_location
-    ball_location.append(location)
+			ROI_red = mask_red[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
+			mean_red = cv2.mean(ROI_red)
+
+			ROI_green = mask_green[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
+			mean_green = cv2.mean(ROI_green)
+
+			ROI_yellow = mask_yellow[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
+			mean_yellow = cv2.mean(ROI_yellow)
+
+			ROI_blue = mask_blue[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
+			mean_blue = cv2.mean(ROI_blue)
+
+			cv2.imshow('mask', mask_blue)
+
+			cv2.circle(img_out,(c[0],c[1]),c[2],(0,255,0),1)
+			# draw the center of the circle
+			cv2.circle(img_out,(c[0],c[1]),2,(0,0,255),3)
+
+			if mean_yellow[0] > 200:
+				#print mean
+				cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (0,255,255), 2)
+				# draw the outer circle
+				cv2.circle(img_out,(c[0],c[1]),c[2],(0,255,255),5)
+				# draw the center of the circle
+				cv2.circle(img_out,(c[0],c[1]),2,(0,255,255),3)
+				location = Vector3(c[0], c[1],c[2])
+				#print (c[0],c[1],c[2])
+				ball_location.append(location)
+
+			if mean_blue[0] > 50:
+				#print mean
+				cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (255,0,0), 2)
+				# draw the outer circle
+				cv2.circle(img_out,(c[0],c[1]),c[2],(255,0,0),5)
+				# draw the center of the circle
+				cv2.circle(img_out,(c[0],c[1]),2,(255,0,0),3)
+				location = Vector3(c[0], c[1],c[2])
+				#print (c[0],c[1],c[2])
+				ball_location.append(location)
+
+			if mean_red[0] > 100:
+				#print mean
+				cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (0,0,255), 2)
+				# draw the outer circle
+				cv2.circle(img_out,(c[0],c[1]),c[2],(0,0,255),5)
+				# draw the center of the circle
+				cv2.circle(img_out,(c[0],c[1]),2,(0,0,255),3)
+				location = Vector3(c[0], c[1],c[2])
+				#print (c[0],c[1],c[2])
+				ball_location.append(location)
+
+			if mean_green[0] > 50:
+				#print mean
+				cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (0,255,0), 2)
+				# draw the outer circle
+				cv2.circle(img_out,(c[0],c[1]),c[2],(0,255,0),5)
+				# draw the center of the circle
+				cv2.circle(img_out,(c[0],c[1]),2,(0,255,0),3)
+				location = Vector3(c[0], c[1],c[2])
+				#print (c[0],c[1],c[2])
+				ball_location.append(location)	
+
 
 while(cap.isOpened()):
     ret, frame = cap.read()
