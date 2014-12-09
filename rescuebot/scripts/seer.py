54e4946a4ecfd6fd043624f4fb18e5247420c85f
Cypress Frankenfeld <iamcypress@gmail.com>
2014-12-09 14:20:17 -0500

    reformated seer

--- rescuebot/scripts/seer.py
+++ rescuebot/scripts/seer.py
@@ -1,139 +1,141 @@
 #!/usr/bin/env python
 #Playing from a file:
-import roslib
-import rospkg
 import sys
+
 import rospy
-import cv2
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import Twist, Vector3
-import math
+from geometry_msgs.msg import Vector3
+
+import cv2
 import numpy as np
 
-class image_converter:
-	def __init__(self):
-		print "I'm initialized!"
-		self.image_pub = rospy.Publisher("/processed_image",Image)
-		self.ball_pub = rospy.Publisher("/ball_coords",Vector3)
 
-		self.bridge = CvBridge()
-		self.image_sub = rospy.Subscriber("/camera/image_raw",Image,self.callback)
-		self.ball_location = None
+class ImageConverter:
+    def __init__(self):
+        print("I'm initialized!")
+        self.image_pub = rospy.Publisher("/processed_image", Image)
+        self.ball_pub = rospy.Publisher("/ball_coords", Vector3)
 
-	def callback(self,data):
-		try:
-			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-		except CvBridgeError, e:
-			print e
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/camera/image_raw", Image, self.callback)
+        self.ball_location = None
 
-		#Image Processing
-		blur = cv2.medianBlur(cv_image,7)
-		gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-		edges = cv2.Canny(gray, 5, 50)
-		self.find_circles(edges,cv_image)
+    def callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
 
-		try:
-			self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-			if self.ball_location:
-				self.ball_pub.publish(self.ball_location)
-		except CvBridgeError, e:
-			print e
+        #Image Processing
+        blur = cv2.medianBlur(cv_image, 7)
+        gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, 5, 50)
+        self.find_circles(edges, cv_image)
 
-	def find_circles(self,img_src,img_out):
-	    """Finds and plots circles using Hough Circle detection."""
-	    circles = cv2.HoughCircles(img_src, cv2.cv.CV_HOUGH_GRADIENT, 1, img_src.shape[0]/8, param1=10, param2=30, minRadius=40, maxRadius=75)
-	    hsv_img = cv2.cvtColor(img_out, cv2.COLOR_BGR2HSV)
+        try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+            if self.ball_location:
+                self.ball_pub.publish(self.ball_location)
+        except CvBridgeError as e:
+            print(e)
 
-	    lower_red = np.array([150, 55, 55])
-	    upper_red = np.array([200, 255, 255])
-	    mask_red = cv2.inRange(hsv_img, lower_red, upper_red)
+    def find_circles(self, img_src, img_out):
+        """Finds and plots circles using Hough Circle detection."""
+        circles = cv2.HoughCircles(img_src, cv2.cv.CV_HOUGH_GRADIENT, 1, img_src.shape[0] / 8, param1=10, param2=30,
+                                   minRadius=40, maxRadius=75)
+        hsv_img = cv2.cvtColor(img_out, cv2.COLOR_BGR2HSV)
 
-	    lower_blue = np.array([100, 55, 55])
-	    upper_blue = np.array([150, 255, 255])
-	    mask_blue = cv2.inRange(hsv_img, lower_blue, upper_blue)
+        lower_red = np.array([150, 55, 55])
+        upper_red = np.array([200, 255, 255])
+        mask_red = cv2.inRange(hsv_img, lower_red, upper_red)
 
-	    lower_yellow = np.array([25, 150, 125])
-	    upper_yellow = np.array([30, 255, 255])
-	    mask_yellow = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
+        lower_blue = np.array([100, 55, 55])
+        upper_blue = np.array([150, 255, 255])
+        mask_blue = cv2.inRange(hsv_img, lower_blue, upper_blue)
 
-	    lower_green = np.array([90, 55, 55])
-	    upper_green = np.array([130, 255, 255])
-	    mask_green = cv2.inRange(hsv_img, lower_green, upper_green)
+        lower_yellow = np.array([25, 150, 125])
+        upper_yellow = np.array([30, 255, 255])
+        mask_yellow = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
 
-	    location = None
-	    if circles is not None:
-	    	for c in circles[0,:]:
-				ROI_red = mask_red[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
-				mean_red = cv2.mean(ROI_red)
+        lower_green = np.array([90, 55, 55])
+        upper_green = np.array([130, 255, 255])
+        mask_green = cv2.inRange(hsv_img, lower_green, upper_green)
 
-				ROI_green = mask_green[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
-				mean_green = cv2.mean(ROI_green)
+        location = None
+        if circles is not None:
+            for c in circles[0, :]:
+                ROI_red = mask_red[c[1] - c[2]:c[1] + c[2], c[0] - c[2]:c[0] + c[2]]
+                mean_red = cv2.mean(ROI_red)
 
-				ROI_yellow = mask_yellow[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
-				mean_yellow = cv2.mean(ROI_yellow)
+                ROI_green = mask_green[c[1] - c[2]:c[1] + c[2], c[0] - c[2]:c[0] + c[2]]
+                mean_green = cv2.mean(ROI_green)
 
-				ROI_blue = mask_blue[c[1]-c[2]:c[1]+c[2], c[0]-c[2]:c[0]+c[2]]
-				mean_blue = cv2.mean(ROI_blue)
+                ROI_yellow = mask_yellow[c[1] - c[2]:c[1] + c[2], c[0] - c[2]:c[0] + c[2]]
+                mean_yellow = cv2.mean(ROI_yellow)
 
-				cv2.circle(img_out,(c[0],c[1]),c[2],(0,255,0),1)
-				# draw the center of the circle
-				cv2.circle(img_out,(c[0],c[1]),2,(0,0,255),3)
+                ROI_blue = mask_blue[c[1] - c[2]:c[1] + c[2], c[0] - c[2]:c[0] + c[2]]
+                mean_blue = cv2.mean(ROI_blue)
 
-				if mean_yellow[0] > 150:
-					#print mean
-					cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (0,255,255), 2)
-					# draw the outer circle
-					cv2.circle(img_out,(c[0],c[1]),c[2],(0,255,255),5)
-					# draw the center of the circle
-					cv2.circle(img_out,(c[0],c[1]),2,(0,255,255),3)
-					location = Vector3(c[0], c[1],c[2])
-					#print (c[0],c[1],c[2])
-					self.ball_location = location
+                cv2.circle(img_out, (c[0], c[1]), c[2], (0, 255, 0), 1)
+                # draw the center of the circle
+                cv2.circle(img_out, (c[0], c[1]), 2, (0, 0, 255), 3)
 
-				if mean_blue[0] > 50:
-					#print mean
-					cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (255,0,0), 2)
-					# draw the outer circle
-					cv2.circle(img_out,(c[0],c[1]),c[2],(255,0,0),5)
-					# draw the center of the circle
-					cv2.circle(img_out,(c[0],c[1]),2,(255,0,0),3)
-					location = Vector3(c[0], c[1],c[2])
-					#print (c[0],c[1],c[2])
-					self.ball_location = location
+                if mean_yellow[0] > 150:
+                    #print mean
+                    cv2.rectangle(img_out, (c[0] - c[2], c[1] + c[2]), (c[0] + c[2], c[1] - c[2]), (0, 255, 255), 2)
+                    # draw the outer circle
+                    cv2.circle(img_out, (c[0], c[1]), c[2], (0, 255, 255), 5)
+                    # draw the center of the circle
+                    cv2.circle(img_out, (c[0], c[1]), 2, (0, 255, 255), 3)
+                    location = Vector3(c[0], c[1], c[2])
+                    #print (c[0],c[1],c[2])
+                    self.ball_location = location
 
-				if mean_red[0] > 100:
-					#print mean
-					cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (0,0,255), 2)
-					# draw the outer circle
-					cv2.circle(img_out,(c[0],c[1]),c[2],(0,0,255),5)
-					# draw the center of the circle
-					cv2.circle(img_out,(c[0],c[1]),2,(0,0,255),3)
-					location = Vector3(c[0], c[1],c[2])
-					#print (c[0],c[1],c[2])
-					self.ball_location = location
+                if mean_blue[0] > 50:
+                    #print mean
+                    cv2.rectangle(img_out, (c[0] - c[2], c[1] + c[2]), (c[0] + c[2], c[1] - c[2]), (255, 0, 0), 2)
+                    # draw the outer circle
+                    cv2.circle(img_out, (c[0], c[1]), c[2], (255, 0, 0), 5)
+                    # draw the center of the circle
+                    cv2.circle(img_out, (c[0], c[1]), 2, (255, 0, 0), 3)
+                    location = Vector3(c[0], c[1], c[2])
+                    #print (c[0],c[1],c[2])
+                    self.ball_location = location
 
-				if mean_green[0] > 50:
-					#print mean
-					cv2.rectangle(img_out, (c[0]-c[2], c[1]+c[2]), (c[0]+c[2],c[1]-c[2]) , (0,255,0), 2)
-					# draw the outer circle
-					cv2.circle(img_out,(c[0],c[1]),c[2],(0,255,0),5)
-					# draw the center of the circle
-					cv2.circle(img_out,(c[0],c[1]),2,(0,255,0),3)
-					location = Vector3(c[0], c[1],c[2])
-					#print (c[0],c[1],c[2])
-					self.ball_location = location	
+                if mean_red[0] > 100:
+                    #print mean
+                    cv2.rectangle(img_out, (c[0] - c[2], c[1] + c[2]), (c[0] + c[2], c[1] - c[2]), (0, 0, 255), 2)
+                    # draw the outer circle
+                    cv2.circle(img_out, (c[0], c[1]), c[2], (0, 0, 255), 5)
+                    # draw the center of the circle
+                    cv2.circle(img_out, (c[0], c[1]), 2, (0, 0, 255), 3)
+                    location = Vector3(c[0], c[1], c[2])
+                    #print (c[0],c[1],c[2])
+                    self.ball_location = location
+
+                if mean_green[0] > 50:
+                    #print mean
+                    cv2.rectangle(img_out, (c[0] - c[2], c[1] + c[2]), (c[0] + c[2], c[1] - c[2]), (0, 255, 0), 2)
+                    # draw the outer circle
+                    cv2.circle(img_out, (c[0], c[1]), c[2], (0, 255, 0), 5)
+                    # draw the center of the circle
+                    cv2.circle(img_out, (c[0], c[1]), 2, (0, 255, 0), 3)
+                    location = Vector3(c[0], c[1], c[2])
+                    #print (c[0],c[1],c[2])
+                    self.ball_location = location
+
 
 def main(args):
-	print 'Theoretically running'
-	rospy.init_node('image_converter', anonymous=True)
-	ic = image_converter()
-	try:
-		print "Hello"
-		rospy.spin()
-	except KeyboardInterrupt:
-		print "Shutting down"
+    print('Theoretically running')
+    rospy.init_node('image_converter', anonymous=True)
+    ic = ImageConverter()
+    try:
+        print("Hello")
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
